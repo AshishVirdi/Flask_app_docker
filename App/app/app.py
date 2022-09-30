6e3490cb083195b8adbaa0727160bd9903d1ab05
AshishVirdi <87603531+AshishVirdi@users.noreply.github.com>
2022-09-30 22:19:26 +0530

    Add files via upload

--- App/app/app.py
+++ App/app/app.py
@@ -17,7 +17,7 @@ def app1():
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip, message = "App2 page")
+        return render_template('index.html', hostname=host_name, ip=host_ip, message = "App1 page")
     except:
         return render_template('error.html')
 
@@ -35,7 +35,7 @@ def admin():
     try:
         host_name = socket.gethostname()
         host_ip = socket.gethostbyname(host_name)
-        return render_template('index.html', hostname=host_name, ip=host_ip, message = "Admin page, should not be accessed")
+        return render_template('index.html', hostname=host_name, ip=host_ip, message = "Admin page (privileged)")
     except:
         return render_template('error.html')
 
